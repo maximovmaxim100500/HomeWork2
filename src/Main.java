e2230c1af403d3b504d4d0351d8f3b244c6c851d
MaximovMaxim <maximovmaxim100500@gmail.com>
2023-10-03 20:57:40 +0300

    исходник

--- src/Main.java
+++ src/Main.java
@@ -50,7 +50,7 @@ public class Main {
         System.out.println(sumMass);
         System.out.println(diffMass);
 
-        System.out.println("Задача 6");
+        System.out.println("Задача 6"); //sfdgttjndtyhgm
 
     }
 }
